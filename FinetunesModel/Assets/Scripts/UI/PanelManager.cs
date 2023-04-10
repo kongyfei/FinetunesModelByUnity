bb338d8904dbcea5c8af65c49bd397048c540317
kyf <1601906881@qq.com>
2023-04-10 10:24:54 +0800

    界面

--- FinetunesModel/Assets/Scripts/UI/PanelManager.cs
+++ FinetunesModel/Assets/Scripts/UI/PanelManager.cs
@@ -8,16 +8,39 @@ using UnityEngine;
 public class PanelManager : MonoSingleton<PanelManager>
 {
     private Stack<PanelBase> panels;
+    private Dictionary<string, GameObject> prefabPool;
+
+    public Transform UIParent;
+    public string rootPath;
 
     protected override void Awake()
     {
         base.Awake();
-
+          
         panels = new Stack<PanelBase>();
+        prefabPool = new Dictionary<string, GameObject>();
     }
 
     public void Show<T>()
     { 
         string typeName = typeof(T).ToString();
+        GameObject prefab;
+        if (!prefabPool.ContainsKey(typeName))
+        {
+            string prefabPath = rootPath + typeName;
+            prefab = Resources.Load(prefabPath) as GameObject;
+            prefabPool.Add(typeName, prefab);
+        }
+        else
+        {
+            prefab = prefabPool[typeName];
+        }
+
+        GameObject panel = Instantiate(prefab, Vector3.zero, Quaternion.identity, UIParent);
+        PanelBase panelBase = panel.GetComponent<PanelBase>();
+        panels.Push(panelBase);
+
+        panelBase.Init();
+        panelBase.Show();
     }
 }
